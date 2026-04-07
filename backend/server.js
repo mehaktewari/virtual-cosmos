805fe5723dfd94235320bdb8ee22e5e68d29542d
mehaktewari <131230377+mehaktewari@users.noreply.github.com>
2026-04-07 18:56:27 +0530

    Initial full project commit

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "*",
+    origin: ["https://virtual-cosmos-eight.vercel.app/", "http://localhost:5173"],
   },
 });
 
